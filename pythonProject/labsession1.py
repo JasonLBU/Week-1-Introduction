6622556a0a52eda321e7c42c1cacf30385853fc1
JasonLBU <114742101+JasonLBU@users.noreply.github.com>
2022-10-25 12:50:11 +0100

    Update labsession1.py

--- pythonProject/labsession1.py
+++ pythonProject/labsession1.py
@@ -1,16 +1,16 @@
-# Task 1
+# Task 1 -  Completed
 print("Hello World")
 
-# Task 2
+# Task 2 - Completed
 print("Hello, Jason")
 
-# Task 3
+# Task 3 - Completed
 Celcius = float(input("What is the temperature (C) today: "))
 Fahrenheit = float((Celcius * 1.8) + 32)
 print(Fahrenheit)
 print()
 
-# Task 4
+# Task 4 - Completed
 matchesPlayed = 609
 battlesPlayed = 1014
 notOut = 162
@@ -20,7 +20,7 @@ BattleAvg = runScore / (battlesPlayed - notOut)
 print("The battle average is" + str(BattleAvg))
 print()
 
-# Task 5
+# Task 5 - Completed
 for x in range(3):
     StudentNum = int(input("How many students are there? "))
     StudentGroup = int(StudentNum // 24)
